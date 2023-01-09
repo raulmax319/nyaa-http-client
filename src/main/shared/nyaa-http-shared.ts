b76aac82434ae83300d766361d219bf29b70a065
Raul Max <raulmax319@gmail.com>
2023-01-09 10:48:24 -0300

    refactor: delegate a class instead of a factory

---
+++ src/main/shared/nyaa-http-shared.ts
@@ -0,0 +1,29 @@
+import { HttpConfigParams } from 'data/protocols/http';
+import { NyaaAnimeCategories, NyaaFilters, NyaaLiteratureCategories } from 'domain/enums';
+import { NyaaModel } from 'domain/models';
+import { makeNyaaService } from 'main/factory';
+import { NyaaService } from 'main/service';
+
+export class NyaaSharedFramework {
+  private readonly service: NyaaService;
+
+  constructor(config: HttpConfigParams) {
+    this.service = makeNyaaService(config);
+  }
+
+  public async findAnime(
+    name: string,
+    category: NyaaAnimeCategories = NyaaAnimeCategories.All,
+    filter: NyaaFilters = NyaaFilters.NoFilter,
+  ): Promise<Array<NyaaModel>> {
+    return await this.service.findAnime(name, category, filter);
+  }
+
+  public async findLiterature(
+    name: string,
+    category: NyaaLiteratureCategories = NyaaLiteratureCategories.All,
+    filter: NyaaFilters = NyaaFilters.NoFilter,
+  ): Promise<Array<NyaaModel>> {
+    return await this.service.findLiterature(name, category, filter);
+  }
+}
