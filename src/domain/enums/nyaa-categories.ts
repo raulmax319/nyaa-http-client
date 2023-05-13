46f130c1f2670f58e8f04b982ca73dcb15ecb28c
Raul Max <raulmax319@gmail.com>
2023-05-13 15:15:44 -0300

    refactor: all lowercase enum cases

--- src/domain/enums/nyaa-categories.ts
+++ src/domain/enums/nyaa-categories.ts
@@ -1,17 +1,19 @@
 export enum NyaaCategories {
-  All = '0_0',
+  all = '0_0',
 }
 
 export enum NyaaAnimeCategories {
-  All = '1_0',
-  English = '1_2',
-  NonEnglish = '1_3',
-  Raw = '1_4',
+  all = '1_0',
+  english = '1_2',
+  nonEnglish = '1_3',
+  'non-english' = NyaaAnimeCategories.nonEnglish,
+  raw = '1_4',
 }
 
 export enum NyaaLiteratureCategories {
-  All = '3_0',
-  English = '3_1',
-  NonEnglish = '3_2',
-  Raw = '3_3',
+  all = '3_0',
+  english = '3_1',
+  nonEnglish = '3_2',
+  'non-english' = NyaaLiteratureCategories.nonEnglish,
+  raw = '3_3',
 }
